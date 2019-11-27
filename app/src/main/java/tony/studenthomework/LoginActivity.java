59252dfcc7cf518939acda0ec0a5a8ff75c08e48
Tony Yang <superzoro168@gmail.com>
2019-11-27 14:30:47 +0800

    Migrate to AndroidX

--- app/src/main/java/tony/studenthomework/LoginActivity.java
+++ app/src/main/java/tony/studenthomework/LoginActivity.java
@@ -2,7 +2,7 @@ package tony.studenthomework;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
