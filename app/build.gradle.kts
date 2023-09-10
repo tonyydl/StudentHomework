adc9d9cda8ad4ee745222f84fa4398efdaf62035
Tony Yang <superzoro168@gmail.com>
2023-09-10 16:54:06 +0800

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tony.studenthomework"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
             type = "String",
