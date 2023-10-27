244edcc5944b97d6dfaece44f9d317e4d0b4278f
Tony Yang <superzoro168@gmail.com>
2023-10-27 13:50:04 +0800

    Add retrofit log for debug

--- app/src/main/java/tony/studenthomework/service/RetrofitManager.kt
+++ app/src/main/java/tony/studenthomework/service/RetrofitManager.kt
@@ -1,14 +1,31 @@
 package tony.studenthomework.service
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import tony.studenthomework.BuildConfig
 
 object RetrofitManager {
 
+    private val logging = HttpLoggingInterceptor().apply {
+        setLevel(
+            if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        )
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BuildConfig.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
         .build()
 
     val studentService: StudentService = retrofit.create(StudentService::class.java)
